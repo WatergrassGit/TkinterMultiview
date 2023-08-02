9b5e4f6e9c9e40da487b3f7a3c763b18245d4fbb
Michael <mwharrisphd@gmail.com>
2023-08-01 23:19:15 -0500

    Basic implementation of object page view.

--- multiview/application.py
+++ multiview/application.py
@@ -14,20 +14,74 @@ class Application(tk.Tk):
 
         self.callbacks = {
             'get_objects': self.get_objects,
+            'set_object': self.set_object,
+            'display_homepage': self.display_homepage,
+            'display_object_view': self.display_object_view,
+            'get_details': self.get_details,
+        }
+
+        # keep track of view object and detail page
+        self.active_view = {
+            'object': None,
+            'detail': None,
         }
 
         self.datamodel = DataModel()
 
-        # initialize views
+        # create view objects
         self.homepage = v.HomePage(self, self.callbacks)
-        self.objectpage = v.ObjectPage()
+        self.objectpage = v.ObjectPage(self, self.callbacks)
         self.detailpage = v.DetailPage()
         self.visibilitypage = v.VisibilityPage()
 
-        # initialize Homepage view
-        self.homepage.pack(fill="both", expand=True)
+        # initialize view
+        self.homepage.grid(row=0, column=0, sticky="nsew")
+        self.objectpage.grid(row=0, column=0, sticky="nsew")
+
+        # configure rows and columns with weight
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        # start with Homepage on top
+        self.show_view(self.homepage)
 
     def get_objects(self):
         """Returns keys from model's data attribute as a list."""
 
         return list(self.datamodel.data.keys())
+    
+    def show_view(self, view):
+        """Displays the inputted view at the top."""
+
+        view.tkraise()
+
+    def display_homepage(self):
+        """Display the Homepage."""
+
+        self.active_view.update({'object': None, 'detail': None})
+        self.show_view(self.homepage)
+
+    def display_object_view(self, obj):
+        """
+        Displays the object view corresponding to input at the top.
+        
+        
+        :arguments
+        ----------
+        obj : string
+            name of asssociated object to be displayed in the Object Page
+        """
+
+        self.objectpage.refresh_page(obj)
+        self.active_view.update({'object': obj, 'detail': None})
+        self.show_view(self.objectpage)
+
+    def set_object(self, obj):
+        self.active_view.update({'object': obj})
+
+    def get_details(self):
+        """
+        Returns keys from model's data attribute for a given object as a list.
+        """
+
+        return list(self.datamodel.data[self.active_view['object']].keys())
--- multiview/views.py
+++ multiview/views.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import ttk
+from functools import partial
 
 
 class HomePage(ttk.Frame):
@@ -15,7 +16,7 @@ class HomePage(ttk.Frame):
         :arguments
         ----------
         master : tkinter object
-            object that MainView resides in
+            object that HomePage resides in
         callbacks : dictionary
             contains references to callable methods in `master`
         """
@@ -34,7 +35,7 @@ class HomePage(ttk.Frame):
         self.frame_btns = []
         for obj in self.objects:
             self.frame_btns.append(
-                ttk.Button(self.frame, text=obj.title())
+                ttk.Button(self.frame, text=obj.title(), command=partial(self.display_object_view, obj))
             )
 
         for btn in self.frame_btns:
@@ -45,9 +46,74 @@ class HomePage(ttk.Frame):
         self.intro.grid(row=1, column=0)
         self.frame.grid(row=2, column=0)
 
+    def display_object_view(self, obj):
+        self.callbacks['display_object_view'](obj)
+
 
 class ObjectPage(ttk.Frame):
-    pass
+    """
+    View for selected object.
+    """
+
+    def __init__(self, master, callbacks, *args, **kwargs):
+        """
+        Initialize Object View
+
+        :arguments
+        ----------
+        master : tkinter object
+            object that ObjectPage resides in
+        callbacks : dictionary
+            contains references to callable methods in `master`
+        """
+
+        super().__init__(master, *args, **kwargs)
+
+        self.callbacks = callbacks
+
+        self.masthead_title_text = tk.StringVar()
+
+        self.masthead = ttk.Frame(self)
+        self.masthead_title = ttk.Label(self.masthead, textvariable=self.masthead_title_text)
+        self.masthead_title.grid(row=0, column=0, sticky='w')
+        self.masthead_button = ttk.Button(self.masthead, text="Home", command=self.callbacks['display_homepage'])
+        self.masthead_button.grid(row=0, column=1, sticky='e')
+        self.masthead.grid_columnconfigure(1, weight=1)
+        self.masthead.grid(row=0, column=0, sticky='nsew')
+
+        # configure rows and columns with weight
+        self.grid_columnconfigure(0, weight=1)
+        self.grid_rowconfigure(0, weight=1)
+
+        # frame to hold buttons
+        self.frame = ttk.Frame(self)
+        self.frame.grid(row=2, column=0)
+
+    def refresh_page(self, obj):
+        self.masthead_title_text.set(f"Object Page - {obj.title()}")
+        self.masthead.grid(row=0, column=0)
+
+        self.callbacks['set_object'](obj)
+
+        # buttons to go into the frame
+        self.details = self.callbacks['get_details']()  # get details as a list
+
+        for widget in self.frame.winfo_children():
+            widget.destroy()
+
+        self.frame_btns = []
+        for det in self.details:
+            self.frame_btns.append(
+                ttk.Button(self.frame, text=det.title(), command=partial(self.display_detail_view, det))
+            )
+
+        for btn in self.frame_btns:
+            btn.pack()
+
+
+    def display_detail_view(self, det):
+        print(det)
+        #self.callbacks['display_detail_view'](det)
 
 
 class DetailPage(ttk.Frame):
